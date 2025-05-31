f14f71177a8078206e31c5518711e89eb20c90d0
WithFortuna <rmsghchl0@naver.com>
2025-05-31 20:25:14 +0900

    [FEAT] 기존 옵션 엔티티 삭제
    OptionCategory & OptionItem 도입: 흩어져있는 옵션 엔티티를 하나의 OptionCategory에서 관리

---
+++ src/main/java/org/hanihome/hanihomebe/property/domain/amenity/AmenityCategory.java
@@ -0,0 +1,29 @@
+/*
+package org.hanihome.hanihomebe.property.domain.amenity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class AmenityCategory {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "amenity_category_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
+*/
