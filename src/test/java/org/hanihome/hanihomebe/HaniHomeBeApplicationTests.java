b6d1d9baad2156564f2115e05abf8c30cedcb423
WithFortuna <rmsghchl0@naver.com>
2025-06-10 17:06:56 +0900

    [FIX] HaniHomeBeApplicationTests 환경변수 업로드 수정
    - 테스트에도 환경변수 적용 가능하도록 코드 수정

--- src/test/java/org/hanihome/hanihomebe/HaniHomeBeApplicationTests.java
+++ src/test/java/org/hanihome/hanihomebe/HaniHomeBeApplicationTests.java
@@ -4,40 +4,24 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HaniHomeBeApplicationTests {
 
     @BeforeAll
-    static void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-        System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
-
-        System.setProperty("DOCKER_SPRING_DATASOURCE_URL", dotenv.get("DOCKER_SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_PROFILES_ACTIVE", dotenv.get("SPRING_PROFILES_ACTIVE"));
-
-        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
-        System.setProperty("JWT_ACCESS_EXP", dotenv.get("JWT_ACCESS_EXP"));
-        System.setProperty("JWT_REFRESH_EXP", dotenv.get("JWT_REFRESH_EXP"));
-
-        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
-        System.setProperty("GOOGLE_REDIRECT_URI", dotenv.get("GOOGLE_REDIRECT_URI"));
-
-
-
+    static void setupEnv() {
+        Dotenv dotenv = Dotenv.configure()
+                .filename(".env.test")
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
     }
-
-
-
-
-
     @Test
-    void contextLoads() {
-    }
+    void hello() {
 
+    }
 }
