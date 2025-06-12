2603619a328e7ac557129c8f733ce1b209764b06
LEE <sw09112@yonsei.ac.kr>
2025-06-12 20:04:48 +0900

    [FIX] springdoc-openapi 버전 충돌로 인한 Swagger 렌더링 오류 해결

--- src/main/java/org/hanihome/hanihomebe/global/config/RedisConfig.java
+++ src/main/java/org/hanihome/hanihomebe/global/config/RedisConfig.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueTemplate;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.convert.MappingRedisConverter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "org.hanihome.hanihomebe.auth.token")
+@EnableRedisRepositories(basePackages = "org.hanihome.hanihomebe.security.auth.application.jwt.refresh")
 public class RedisConfig {
-    @Value("${REDIS_HOST}")
+    @Value("${spring.data.redis.host}")//이거 안해도 된다는 정보가 있는데 불안해서 넣어둡니다
     private String redisHost;
 
     @Bean
@@ -19,6 +22,7 @@ public class RedisConfig {
 
         return new LettuceConnectionFactory(redisHost, 6379); // 호스트/포트 설정 가능 TODO: 호스트 환경변수로 관리 필요.
     }
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
