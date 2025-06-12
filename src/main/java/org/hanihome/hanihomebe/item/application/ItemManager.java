ec4ae60fcfdd11751eda8ab0eb3056d9da61b22c
WithFortuna <rmsghchl0@naver.com>
2025-06-12 20:24:29 +0900

    [FEAT] 아이템 이니셜라이징 기능 추가
    - OptionItemManager -> ItemManger로 이름 변경
    : OptionItem뿐만 아니라 ScopeType, OptionCategoryScope 등도 같이 관리하는 책임을 지고 있기에 변경하였음
    
    - ScopeType, OptionCategoryScope 초기화 기능 추가
    : ScopeInitializer
    
    - 매물 카테고리 및 아이템 초기화 기능 추가
    : PropertyItemInitializer
    - 뷰잉 카테고리 및 아이템 초기화 기능 추가
    : ViewingItemInitializer

--- src/main/java/org/hanihome/hanihomebe/item/application/ItemManager.java
+++ src/main/java/org/hanihome/hanihomebe/item/application/ItemManager.java
@@ -1,7 +1,10 @@
 package org.hanihome.hanihomebe.item.application;
 
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,14 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class OptionItemManager {
+public class ItemManager implements ApplicationRunner {
     private final PropertyItemInitializer propertyItemInitializer;
     private final ViewingItemInitializer viewingItemInitializer;
+    private final ScopeInitializer scopeInitializer;
 
     @Transactional
-    @PostConstruct
-    public void init() {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        /// initialize optionItems
         propertyItemInitializer.initialize();
         viewingItemInitializer.initialize();
+
+        /// initialize scope and OptionCategoryScope
+        scopeInitializer.initialize();
     }
 }
