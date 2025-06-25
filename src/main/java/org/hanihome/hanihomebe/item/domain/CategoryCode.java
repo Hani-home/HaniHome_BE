53bbbbb8e110d28b2c4d6f6426b6b163c7e58f6c
WithFortuna <rmsghchl0@naver.com>
2025-06-25 22:05:17 +0900

    [FEAT] OptionItem 카테고리 순서 변경

--- src/main/java/org/hanihome/hanihomebe/item/domain/CategoryCode.java
+++ src/main/java/org/hanihome/hanihomebe/item/domain/CategoryCode.java
@@ -7,10 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum CategoryCode {
     /// 매물 카테고리
-    PROPERTY_CAT1("빌에 포함된 항목"),
-    PROPERTY_CAT2("다음 항목이 가능한지 선택해주세요"),
-    PROPERTY_CAT3("기본 제공 가전/가구"),
-    PROPERTY_CAT4("매물 장점"),
+    PROPERTY_CAT1("매물의 장점은 무엇인가요"),
+    PROPERTY_CAT2("기본 제공 가전/가구를 선택해주세요"),
+    PROPERTY_CAT3("다음 항목이 가능한지 선택해주세요"),
+    PROPERTY_CAT4("빌에 포함된 항목"),
     PROPERTY_CAT5("부동산 중개 여부를 알려주세요"),
 
     /// 뷰잉 카테고리
