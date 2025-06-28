ab93538f92360d22c4505935600d49747c3bed70
WithFortuna <rmsghchl0@naver.com>
2025-06-28 15:06:32 +0900

    [FEAT] 매물 필터링 searchRepository 구현
    - 매물 조건에 따른 동적 쿼리 작성
    - 다형성을 사용하여 PropertyRepository는 JpaRepository, PropertySearchRepository 다중 상속

---
+++ src/main/java/org/hanihome/hanihomebe/property/web/controller/PropertySearchController.java
@@ -0,0 +1,30 @@
+package org.hanihome.hanihomebe.property.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.hanihome.hanihomebe.property.application.service.PropertySearchService;
+import org.hanihome.hanihomebe.property.domain.enums.PropertySuperType;
+import org.hanihome.hanihomebe.property.web.dto.PropertySearchConditionDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.PropertyResponseDTO;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/properties")
+@RestController
+public class PropertySearchController {
+    private final PropertySearchService propertySearchService;
+
+    @GetMapping("/search")
+    public Map<PropertySuperType,List<PropertyResponseDTO>> searchProperties(
+            @ModelAttribute PropertySearchConditionDTO conditionDTO,
+            Pageable pageable) {
+        return propertySearchService.search(conditionDTO);
+    }
+}
