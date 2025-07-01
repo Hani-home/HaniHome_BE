09c5f567fe75e47dc00d483d718d9595b09cb95c
WithFortuna <rmsghchl0@naver.com>
2025-07-02 00:01:07 +0900

    [REFACTOR] 테스트코드 - 매물의 뷰잉가능시간 반영

--- src/test/java/org/hanihome/hanihomebe/property/application/service/WishItemServiceTest.java
+++ src/test/java/org/hanihome/hanihomebe/property/application/service/WishItemServiceTest.java
@@ -6,6 +6,7 @@ import org.hanihome.hanihomebe.member.domain.Member;
 import org.hanihome.hanihomebe.member.domain.Role;
 import org.hanihome.hanihomebe.member.repository.MemberRepository;
 import org.hanihome.hanihomebe.property.domain.ShareProperty;
+import org.hanihome.hanihomebe.property.domain.TimeSlot;
 import org.hanihome.hanihomebe.property.domain.enums.CapacityShare;
 import org.hanihome.hanihomebe.property.domain.enums.GenderPreference;
 import org.hanihome.hanihomebe.property.domain.enums.ParkingOption;
@@ -28,6 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Set;
 
@@ -70,7 +72,11 @@ public class WishItemServiceTest {
 
         memberId = memberRepository.save(member).getId();
 
-        Region region = new Region("Australia", "2067", "NSW", "Chatswood", "Smith St", "25", "1203", "Chatswood Central Apartments");
+        /*
+         * 매물 종류 / 매물 유형 / 예산 범위 / 입주 가능일 / 지하철역
+         *
+         */
+        Region region = new Region("Australia", "2067", "NSW", "Chatswood", "Smith St", "25", "1203", "Chatswood Central Apartments", BigDecimal.valueOf(150), BigDecimal.valueOf(0));
         List<String> photoUrls = List.of("https://example.com/1.jpg", "https://example.com/2.jpg");
         BigDecimal deposit = new BigDecimal("800.00");
         BigDecimal keyDeposit = new BigDecimal("100.00");
@@ -79,6 +85,11 @@ public class WishItemServiceTest {
         LocalDateTime availableTo = LocalDateTime.of(2025, 5, 10, 5, 5);
 
         BigDecimal weeklyCost = new BigDecimal("100.00");
+
+        // 뷰잉 가능 날짜
+        List<TimeSlot> timeSlots = List.of(new TimeSlot(LocalTime.of(12, 0), LocalTime.of(12, 30)), new TimeSlot(LocalTime.of(15, 0), LocalTime.of(15, 30)));
+        LocalDate meetingDateFrom = LocalDate.of(2025, 5, 10);
+        LocalDate meetingDateTo = LocalDate.of(2025, 5, 12);
 
         SharePropertyCreateRequestDTO dto = new SharePropertyCreateRequestDTO(
                 memberId, // memberId
@@ -100,7 +111,9 @@ public class WishItemServiceTest {
                 true,
                 true,
                 ParkingOption.STREET_PARKING,
-                Set.of(LocalDateTime.of(2025, 6, 30, 14, 0)),
+                meetingDateFrom,
+                meetingDateTo,
+                timeSlots,
                 "깨끗하고 조용한 마스터룸입니다.",
                 SharePropertySubType.MASTER_ROOM,
                 10.0, // internalArea
@@ -111,7 +124,6 @@ public class WishItemServiceTest {
                 2,
                 CapacityShare.DOUBLE
         );
-
         ShareProperty shareProperty = ShareProperty.create(dto, member);
         propertyId = propertyRepository.save(shareProperty).getId();
 
