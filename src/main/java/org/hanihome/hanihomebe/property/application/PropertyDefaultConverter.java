f656ad7f5d4295ddfe59924ad01b902c3fba2366
WithFortuna <rmsghchl0@naver.com>
2025-07-11 09:58:16 +0900

    [FEAT] ViewType 별로 매물 DTO 반환 - Converter
    - ViewType별 응답 DTO를 결정하기 위해 PropertyConverter 다형성 활용

---
+++ src/main/java/org/hanihome/hanihomebe/property/application/PropertyDefaultConverter.java
@@ -0,0 +1,57 @@
+package org.hanihome.hanihomebe.property.application;
+
+
+import org.hanihome.hanihomebe.global.exception.CustomException;
+import org.hanihome.hanihomebe.global.response.domain.ServiceCode;
+import org.hanihome.hanihomebe.item.web.dto.OptionItemResponseDTO;
+import org.hanihome.hanihomebe.property.application.PropertyConverter;
+import org.hanihome.hanihomebe.property.domain.Property;
+import org.hanihome.hanihomebe.property.domain.RentProperty;
+import org.hanihome.hanihomebe.property.domain.ShareProperty;
+import org.hanihome.hanihomebe.property.domain.enums.PropertySuperType;
+import org.hanihome.hanihomebe.property.web.dto.enums.PropertyViewType;
+import org.hanihome.hanihomebe.property.web.dto.response.PropertyResponseDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.RentPropertyResponseDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.SharePropertyResponseDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.summary.PropertySummaryDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.summary.RentPropertySummaryDTO;
+import org.hanihome.hanihomebe.property.web.dto.response.summary.SharePropertySummaryDTO;
+import org.hibernate.Hibernate;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PropertyDefaultConverter implements PropertyConverter<PropertyResponseDTO> {
+    @Override
+    public PropertyViewType supports() {
+        return PropertyViewType.DEFAULT;
+    }
+
+    @Override
+    public PropertyResponseDTO convert(Property property, List<OptionItemResponseDTO> optionItems) {
+        return toResponseDTO(property, optionItems);
+    }
+
+
+    PropertyResponseDTO toResponseDTO(Property entity, List<OptionItemResponseDTO> optionItems) {
+        PropertySuperType propertyType = entity.getKind();
+        switch (propertyType) {
+            case SHARE:
+                return SharePropertyResponseDTO.from(safeCast(entity, ShareProperty.class), optionItems);
+            case RENT:
+                return RentPropertyResponseDTO.from(safeCast(entity, RentProperty.class), optionItems);
+            default:
+                throw new CustomException(ServiceCode.INVALID_PROPERTY_TYPE);
+        }
+    }
+
+    private <T> T safeCast(Object entity, Class<T> targetClass) {
+        if (entity instanceof HibernateProxy) {
+            return targetClass.cast(Hibernate.unproxy(entity));
+        }
+        return targetClass.cast(entity);
+
+    }
+}
