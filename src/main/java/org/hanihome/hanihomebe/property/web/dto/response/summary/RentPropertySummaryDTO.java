04c74318cb8bb35667a2e40a7a4ef70e33deba29
WithFortuna <rmsghchl0@naver.com>
2025-07-15 10:46:01 +0900

    [REFACTOR] PropertySummaryDTO 필드 추가 - kind

--- src/main/java/org/hanihome/hanihomebe/property/web/dto/response/summary/RentPropertySummaryDTO.java
+++ src/main/java/org/hanihome/hanihomebe/property/web/dto/response/summary/RentPropertySummaryDTO.java
@@ -1,6 +1,7 @@
 package org.hanihome.hanihomebe.property.web.dto.response.summary;
 
 import org.hanihome.hanihomebe.property.domain.RentProperty;
+import org.hanihome.hanihomebe.property.domain.enums.PropertySuperType;
 import org.hanihome.hanihomebe.property.domain.enums.RentPropertySubType;
 import org.hanihome.hanihomebe.property.domain.enums.TradeStatus;
 import org.hanihome.hanihomebe.property.web.dto.response.PropertyDTOByView;
@@ -10,12 +11,13 @@ import java.time.LocalDateTime;
 
 public record RentPropertySummaryDTO(
         Long id,
+        PropertySuperType kind,
+        RentPropertySubType propertySubType,
         BigDecimal weeklyCost,
         String suburb,
         Double internalArea,
         int totalFloors,
         boolean billIncluded,
-        RentPropertySubType propertySubType,
         String thumbnailUrl,
         LocalDateTime createdAt,
         int wishCount,
@@ -24,12 +26,13 @@ public record RentPropertySummaryDTO(
     public static RentPropertySummaryDTO from(RentProperty entity) {
         return new RentPropertySummaryDTO(
                 entity.getId(),
+                entity.getKind(),
+                entity.getRentPropertySubType(),
                 entity.getCostDetails().getWeeklyCost(),
                 entity.getRegion().getSuburb(),
                 entity.getRentInternalDetails().getInternalArea(),
                 entity.getRentInternalDetails().getTotalFloors(),
                 entity.getCostDetails().isBillIncluded(),
-                entity.getRentPropertySubType(),
                 entity.getThumbnailUrl(),
                 entity.getCreatedAt(),
                 entity.getWishCount(),
