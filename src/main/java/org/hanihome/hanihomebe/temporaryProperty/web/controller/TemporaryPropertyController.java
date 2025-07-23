1ad48e06f7c1fb7592435b61d458234a65ec6f92
LEE <sw09112@yonsei.ac.kr>
2025-07-23 22:56:04 +0900

    [FEAT] 생성, 업데이트 컨트롤러 작성

--- src/main/java/org/hanihome/hanihomebe/temporaryProperty/web/controller/TemporaryPropertyController.java
+++ src/main/java/org/hanihome/hanihomebe/temporaryProperty/web/controller/TemporaryPropertyController.java
@@ -1,13 +1,46 @@
 package org.hanihome.hanihomebe.temporaryProperty.web.controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.hanihome.hanihomebe.security.auth.user.detail.CustomUserDetails;
+import org.hanihome.hanihomebe.temporaryProperty.application.TemporaryPropertyService;
+import org.hanihome.hanihomebe.temporaryProperty.web.dto.TemporaryPropertyStepSaveRequestDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/temporary-properties")
+@RequiredArgsConstructor
 public class TemporaryPropertyController {
-    //생성@PostMapping
 
-    //수정@PatchMapping("/temporary-properties/{id}")
+    private final TemporaryPropertyService temporaryPropertyService;
+
+
+    @PostMapping
+    public ResponseEntity<Void> createTemporaryProperty(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody TemporaryPropertyStepSaveRequestDTO dto) {
+        Long hostId = userDetails.getUserId();
+        temporaryPropertyService.temporaryPropertyCheckAndSave(hostId, dto, null);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PatchMapping("/{temporaryPropertyId}")
+    public ResponseEntity<Void> updateTemporaryProperty(
+            @PathVariable Long temporaryPropertyId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody TemporaryPropertyStepSaveRequestDTO dto
+    ) {
+        Long hostId = userDetails.getUserId();
+        temporaryPropertyService.temporaryPropertyCheckAndSave(hostId, dto, temporaryPropertyId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+
 }
