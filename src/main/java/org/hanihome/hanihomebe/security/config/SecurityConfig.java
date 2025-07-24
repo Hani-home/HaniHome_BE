8fadc44fea1449b09b5343fd59bb8092d3cf64cf
LEE <sw09112@yonsei.ac.kr>
2025-07-24 13:41:30 +0900

    CHORE: 비로그인 사용자의 검색 API(/properties/search) 접근 허용 설정

--- src/main/java/org/hanihome/hanihomebe/security/config/SecurityConfig.java
+++ src/main/java/org/hanihome/hanihomebe/security/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET,
                                 "/api/v1/properties/categories/**",
                                 "/api/v1/viewings/categories/**",
-                                "/api/v1/properties"
+                                "/api/v1/properties",
+                                "api/v1/properties/search"
                         ).permitAll()
                         .requestMatchers(
                                 "/api/v1/auth/social/login",
