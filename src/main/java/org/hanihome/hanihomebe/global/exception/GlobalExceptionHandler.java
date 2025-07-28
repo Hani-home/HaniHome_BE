55febf0657833dab94bbc6ade277389b2b1b4af8
WithFortuna <rmsghchl0@naver.com>
2025-07-29 01:04:25 +0900

    [FIX] 매물 삭제 연관된 엔티티 모두 삭제. soft delete는 추후 고려

--- src/main/java/org/hanihome/hanihomebe/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/hanihome/hanihomebe/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.hanihome.hanihomebe.global.response.domain.ServiceCode;
 import org.hanihome.hanihomebe.global.response.dto.CommonResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -33,4 +34,24 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(serviceCode.getHttpStatus().value())
                 .body(commonResponse);
     }
+
+    // FK 제약 위반 시 처리
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<CommonResponse> handleDataIntegrityViolation(
+            DataIntegrityViolationException e, HttpServletRequest request) {
+        // API-docs 예외 처리 제외
+        if (request.getRequestURI().startsWith("/v3/api-docs")) {
+            return ResponseEntity.ok().build();
+        }
+
+        log.error("DataIntegrityViolationException 발생: {}", e.getMessage(), e);
+        ServiceCode serviceCode = ServiceCode.DELETE_FAILED_HAS_RELATION;
+
+        CommonResponse<ErrorResponseDTO> commonResponse =
+                CommonResponse.failure(ErrorResponseDTO.of(serviceCode), serviceCode);
+        return ResponseEntity
+                .status(serviceCode.getHttpStatus().value())
+                .body(commonResponse);
+    }
+
 }
