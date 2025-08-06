3f4a63e6ce8f868436692228382f77d798afd1ae
WithFortuna <rmsghchl0@naver.com>
2025-08-06 16:08:01 +0900

    [FEAT] timeSlot 전처리 기능 추가 - 모두 null인 경우 무시
    기존 tiemSlot에서 (from,to)가 모두 null인 경우는 해당 slot을 삭제한 processedTimeSlot 생성.
    
    기존: 둘 중에 하나라도 null 이 있다면 오류 발생
    변경: 둘 다 null 인 경우는 허용 및 무시. 둘중에 하나만 null인 경우는 오류발생

--- src/main/java/org/hanihome/hanihomebe/property/application/time/PropertyCreateTimeManager.java
+++ src/main/java/org/hanihome/hanihomebe/property/application/time/PropertyCreateTimeManager.java
@@ -10,30 +10,31 @@ import org.hanihome.hanihomebe.property.domain.vo.ViewingAvailableDateTime;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PropertyCreateTimeManager {
+    public static List<TimeSlot> preprocessTimeSlots(List<TimeSlot> timeSlots) {
+        if (timeSlots == null) {
+            return Collections.emptyList();
+        }
+
+        return timeSlots.stream()
+                .filter(timeSlot -> !(timeSlot.getTimeFrom() == null && timeSlot.getTimeTo() == null))
+                .peek(timeSlot -> {
+                    if (timeSlot.getTimeFrom() == null || timeSlot.getTimeTo() == null) {
+                        throw new CustomException(ServiceCode.INVALID_PROPERTY_TIME_SLOT);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
     public static List<ViewingAvailableDateTime> validateAndGenerateViewingAvailableDateTimes(List<TimeSlot> timeSlots,
                                                                                               MeetingDatePeriod meetingDatePeriod) {
-        // 뷰잉 가능 시간 검증
         validateTimeSlots(timeSlots);
-
-        // ViewingAvailableDateTime 변환
         List<ViewingAvailableDateTime> generated = generateViewingAvailableDateTimes(timeSlots, meetingDatePeriod);
         return generated;
-    }
-
-
-    public static List<ViewingAvailableDateTime> validateAndGenerateTwoMonthsOfViewingAvailableDateTimes(List<TimeSlot> timeSlots, MeetingDatePeriod meetingDatePeriod) {
-        validateTimeSlots(timeSlots);
-        return generateViewingAvailableDateTimes(timeSlots, meetingDatePeriod);
-    }
-
-    private static void validateTimeSlots(List<TimeSlot> timeSlots) {
-        boolean isValidTimeSlots = TimeSlotValidator.validateAllConditions(timeSlots);
-        if(!isValidTimeSlots) {
-            throw new CustomException(ServiceCode.INVALID_PROPERTY_TIME_SLOT);
-        }
     }
 
     public static MeetingDatePeriod buildTwoMonths() {
@@ -43,6 +44,13 @@ public class PropertyCreateTimeManager {
         LocalDate meetingDateTo = meetingDateFrom.plusMonths(2);
         MeetingDatePeriod meetingDatePeriod = MeetingDatePeriod.create(meetingDateFrom, meetingDateTo);
         return meetingDatePeriod;
+    }
+
+    private static void validateTimeSlots(List<TimeSlot> timeSlots) {
+        boolean isValidTimeSlots = TimeSlotValidator.validateAllConditions(timeSlots);
+        if(!isValidTimeSlots) {
+            throw new CustomException(ServiceCode.INVALID_PROPERTY_TIME_SLOT);
+        }
     }
 
     private static List<ViewingAvailableDateTime> generateViewingAvailableDateTimes(List<TimeSlot> timeSlots, MeetingDatePeriod meetingDatePeriod) {
